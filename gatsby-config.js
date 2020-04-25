d2d23f6ccb2b4ec219aef473d4c7dfa92545be2e
Dean Iverson <diverson@vtti.vt.edu>
2020-04-25 13:01:23 -0600

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Pleasing Software`,
+    description: `A quick and simple web site`,
+    author: `Dean Iverson <dean@pleasingsoftware.com>`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
