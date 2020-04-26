a5cac6ed142ff8c605afcd0cfb186ca13e91d5cf
Dean Iverson <diverson@vtti.vt.edu>
2020-04-25 18:09:04 -0600

    Adjusted image size

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const rootStyle = {
+  width: "100%",
   height: '100%',
   display: 'flex',
   flexDirection: 'column',
@@ -16,7 +17,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home"/>
     <div style={rootStyle}>
-      <div style={{ width: 1024, height: 734 }}>
+      <div style={{ width: "80%", height: "100%", maxWidth: 1024, maxHeight: 1024 }}>
         <Image/>
       </div>
     </div>
